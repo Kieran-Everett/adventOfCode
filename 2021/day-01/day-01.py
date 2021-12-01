7d1816ed3eb4b5c0ddfe0a0210dc99004dd516ef
Kieran Everett <kieraneverett03@gmail.com>
2021-12-01 19:35:29 +0000

    Solved part 2

--- 2021/day-01/day-01.py
+++ 2021/day-01/day-01.py
@@ -13,3 +13,28 @@ for number in f:
         previous = int(number)
 
 print(increases)
+f.close()
+
+
+f = open("2021/day-01/day-01-input")
+previous = 0
+increases = 0
+count = 0
+array = []
+firstSection = True
+
+for number in f:
+    array.append(int(number))
+    if firstSection:
+        if count > 2:
+            firstSection = False
+    else:
+        previous = array[count-3] + array[count-2] + array[count-1]
+        current = array[count-2] + array[count-1] + array[count]
+        if current > previous:
+            increases += 1
+
+    count += 1
+
+print(increases) # one below for some reason lol
+f.close()
