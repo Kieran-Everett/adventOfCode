26033f4ee27fc42c0aed24c61032a32f11485f97
Kieran Everett <kieraneverett03@gmail.com>
2021-12-02 16:31:20 +0000

    Day 2 part 2

--- 2021/day-02/day-02.py
+++ 2021/day-02/day-02.py
@@ -12,3 +12,23 @@ for line in f:
         y -= int(movement[1])
 
 print(x*y)
+f.close()
+
+
+f = open("2021/day-02/day-02-input")
+x = 0
+y = 0
+aim = 0
+
+for line in f:
+    movement = line.split()
+    if movement[0] == "forward":
+        x += int(movement[1])
+        y += int(movement[1]) * aim
+    elif movement[0] == "down":
+        aim += int(movement[1])
+    elif movement[0] == "up":
+        aim -= int(movement[1])
+
+print(x*y)
+f.close()
